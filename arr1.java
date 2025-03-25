8f1777b56c41f2e8fd5aec2aeb0fa0768e807e3c
sujoy98 <sujoysarkar951@gmail.com>
2025-03-26 01:19:18 +0530

    26th logoff

---
+++ arr1.java
@@ -0,0 +1,11 @@
+public class arr1 {
+    public static void main(String[] args) {
+        int ar[]=new int[5];
+        for(int i=0;i<5;i++){
+            ar[i]=i;
+        }
+        for(int j=0;j<5;j++){
+            System.out.println(ar[j]);
+        }
+    }
+}
